40c86f631311f0cd5ae7bc63a3c3db71aa790337
vtic22 <vtic22@gmail.com>
2017-06-22 11:40:40 +0200

    Initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: httpd
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+
+
+package 'httpd'
+
+file '/var/www/html/index.html' do 
+  content '<h1> Welcome Home!<h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
